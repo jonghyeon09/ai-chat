d30f743dd78f28f11c40e3b3e1d8a716c869cc60
jonghyeon <cjddnjfql@naver.com>
2023-06-23 19:32:23 +0900

    chor: seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import Layout from '@/components/chat/Layout';
 import ChatSection from '@/components/chat/ChatSection';
+import { NextSeo } from 'next-seo';
 
 export default function Home() {
   return (
-    <Layout>
-      <ChatSection />
-    </Layout>
+    <>
+      <NextSeo
+        title="AI 채팅앱"
+        description="KoGPT와 대화하는 사이트 입니다."
+      />
+      <Layout>
+        <ChatSection />
+      </Layout>
+    </>
   );
 }
